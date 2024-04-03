2a6c84662b8c9b31612e6b0f5318443cdaf94d78
Henry Arnold <devharnold@gmail.com>
2024-04-03 22:50:24 +0300

    make change

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python3
-"""fifo cache module"""
+"""lifo cache module"""
 from collections import OrderedDict
 
 BaseCaching = __import__('base_caching').BaseCaching
 
-class FIFOCache(BaseCaching):
-    """class caching system"""
+class LIFOCache(BaseCaching):
+    """class LIFOCache"""
     def __init__(self, capacity):
-        """Initialize class fifocache"""
+        """initialize"""
         super().__init__()
         self.capacity = capacity
-        self.cache = OrderedDict()
+        self.cache = OrderedDict
 
         self.cache_data = {}
-        
 
-    def put(self, key, item):
+    def put(self, key, item) -> dict:
         """
         must assign to the dictionary `self.cache_data` the item value for the key `key`
         if `key` or `item` is None -> do nothing
         if number of items in self.cache_data > `BaseCaching.MAX_ITEMS`:
-            ->must discard the first item put in cache(FIFO algorithm)
+            ->must discard the last item put in cache(LIFO algorithm)
             ->must print `DISCARD` with the key dicarded
         Attributes:
             item: element
             key: caching steps
         """
         if len(self.cache) >= self.capacity:
-            if key not in self.cache:
+            if(key, item)not in self.cache:
                 discard = next(iter(self.cache))
-            del self.cache[discard]
+                del self.cache[discard]
             self.cache[key] = item
 
     def get(self, key):
