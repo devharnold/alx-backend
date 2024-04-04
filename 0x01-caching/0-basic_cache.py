666644db3b0d2c766ec7346b095b341f37e0b63f
Henry Arnold <devharnold@gmail.com>
2024-04-04 10:08:03 +0300

    make correction

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python3
 """basic cache module"""
 
-
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
-    """
-    class caching system
-    """
-    def __init__(self):
-        """
-        Initialize
-        """
-        self.cache_data = {}
 
     def put(self, key, item):
         """Must assign the dictionary
             self.cache_data -> the item value for the key
         Method should not do anything
         """
-        for key, items in sorted(self.cache_data.keys()):
-            if (key, items) is None:
-                raise NotImplementedError("Cannot work with empty keys and items")
+        if key and item:
+            self.cache_data[key] = item
+        else:
+            return None
             
     def get(self, key):
         """
         Must return the value in `self.cache_data` linked to key
         Return None: if key -> None or not existing in self.cache_data
         """
-        if key in self.cache:
-            return self.cache[key]
-        return None
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
