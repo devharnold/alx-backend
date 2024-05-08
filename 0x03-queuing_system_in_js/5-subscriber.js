fef33e9299b33b7b7063dd719815168b55ac55da
Henry Arnold <devharnold@gmail.com>
2024-05-09 00:44:41 +0300

    subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,17 @@
+import { createClient } from 'redis';
+import { promisify } from 'Util';
+const redis = require('redis');
+
+const client = createClient().on('error', (err) => 
+    console.log('Redis client not connected to the server:', err.message)
+);
+console.log('Redis has connected to the server');
+
+client.subscribe('holberton school');
+client.on('message', (channel, message) => {
+    console.log('Received message on channel ${channel}: ${message}');
+    if (message === 'KILL_SERVER') {
+        client.unsubscribe();
+        client.quit();
+    }
+});
